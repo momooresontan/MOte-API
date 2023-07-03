ea91885e3d7bb80a1004b9898d23cfd0ecbd0f4a
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-07-03 17:32:32 +0100

    user route modified & user controller created

---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../nodels/userModel");
+
+exports.getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find();
+  if (!users) {
+    res.status(404);
+    throw new Error("No users found!");
+  }
+  res.status(200).json(users);
+});
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const { getAllUsers } = require("../controllers/userController");
 
 const router = express.Router();
 
-router.get("/");
+router.get("/", getAllUsers);
 
 router.post("/signup");
 router.post("/login");
