e4f99aef4a54a9f97c4d447b930087a31a3a722f
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-07-03 17:10:52 +0100

    user & mote model created

--- models/moteModel.js
+++ models/moteModel.js
@@ -1,3 +1,8 @@
 const mongoose = require("mongoose");
 
-const moteSchema = new mongoose.Schema({});
+const moteSchema = new mongoose.Schema({
+  mote: {
+    type: String,
+    required: [true, "Say something"],
+  },
+});
