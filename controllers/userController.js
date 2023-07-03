0d9b61d15ed11384b0c713bb4ea3e6f072346050
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-07-03 20:48:07 +0100

    user controller modified

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,23 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
 const User = require("../models/userModel");
 
 exports.getAllUsers = asyncHandler(async (req, res) => {
   const users = await User.find();
   if (!users) {
-    res.status(404);
-    throw new Error("No users found!");
+    res.status(404).json({ message: "No users found!" });
   }
   res.status(200).json(users);
+});
+
+exports.signup = asyncHandler(async (req, res) => {
+  const { username, email, password, role, motes } = req.body;
+  if(!username || !email)
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    res.status(400).json({ message: "User already exists! Login instead" });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = 
 });
