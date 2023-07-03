cfd737902c0b8f92e46c83bde3881f82e56c26dc
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-07-04 00:10:24 +0100

    like model modified

--- models/likeModel.js
+++ models/likeModel.js
@@ -1,13 +1,27 @@
 const mongoose = require("mongoose");
 
-const likeSchema = new mongoose.Schema({
-  liked: {
-    type: Boolean,
-    default: false,
+const likeSchema = new mongoose.Schema(
+  {
+    liked: {
+      type: Boolean,
+      default: false,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Like must have a user!"],
+    },
+    mote: {
+      type: mongoose.Types.ObjectId,
+      ref: "Motes",
+      required: [true, "Like must belong to a mote!"],
+    },
   },
-  user: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-    required: [true, "Like must have a user!"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const Like = mongoose.model("Like", likeSchema);
+
+module.exports = Like;
